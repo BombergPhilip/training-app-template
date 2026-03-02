c86b321763b218d8ff1df89b7277dba0e8721ad8
Wame Gassama <161361272+WameGassama@users.noreply.github.com>
2026-03-02 13:02:04 +0100

    fix: removed unused files

--- packages/web/src/components/ui/input-group.tsx
+++ packages/web/src/components/ui/input-group.tsx
@@ -22,13 +22,6 @@ function InputGroup({ className, ...props }: React.ComponentProps<"div">) {
         "has-[>[data-align=inline-end]]:[&>input]:pr-2",
         "has-[>[data-align=block-start]]:h-auto has-[>[data-align=block-start]]:flex-col has-[>[data-align=block-start]]:[&>input]:pb-3",
         "has-[>[data-align=block-end]]:h-auto has-[>[data-align=block-end]]:flex-col has-[>[data-align=block-end]]:[&>input]:pt-3",
-
-        // Focus state.
-        // "has-[[data-slot=input-group-control]:focus-visible]:border-blue-500 has-[[data-slot=input-group-control]:focus-visible]:ring-[3px] has-[[data-slot=input-group-control]:focus-visible]:ring-blue-500/50",
-
-        // Error state.
-        // "has-[[data-slot][aria-invalid=true]]:ring-destructive/20 has-[[data-slot][aria-invalid=true]]:border-destructive dark:has-[[data-slot][aria-invalid=true]]:ring-destructive/40",
-
         className,
       )}
       {...props}
